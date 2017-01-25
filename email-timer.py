8a3b2092de66a6ae6febc395cc9dcfb8f11f5912
ektavarshney22 <ektavarshney22@gmail.com>
2017-01-25 18:03:50 +0530

    Add files via upload

---
+++ email-timer.py
@@ -0,0 +1,28 @@
+import time
+import smtplib
+run = input("Start? > ")
+mins = 0
+
+
+if run == "start":
+    
+    while mins != 20:
+        
+        # Sleep for a minute
+        time.sleep(5)
+        to = ''
+        gmail_user=""
+        gmail_pwd=""
+        smtpserver=smtplib.SMTP("smtp.gmail.com",587)
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.ehlo
+        smtpserver.login(gmail_user, gmail_pwd)
+        msg = "hehhaha made it! yo budhu!"
+        smtpserver.sendmail(gmail_user, to, msg)
+        print('done')
+        smtpserver.close()
+        # Increment the minute total
+        mins += 1
+
+
